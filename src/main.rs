a5dae5df67e087a8f139d55a4b60745420a2cc46
zrdzn <zrdzn9@gmail.com>
2024-10-26 15:20:56 +0200

    Init project with rust

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
